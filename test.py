39ff430837213485f29089142f7ed02c3893ce15
mirac20116161 <168637550+mirac20116161@users.noreply.github.com>
2024-05-29 22:27:42 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,27 +1,35 @@
 import discord
 from discord.ext import commands
+import random
+import os
+import requests
+
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='$', intents=intents)
+client = discord.Client(intents=intents)
 
-@bot.event
+@client.event
 async def on_ready():
-    print(f'{bot.user} olarak giriş yaptık')
+    print(f'{client.user} olarak giriş yaptık')
 
-@bot.command()
-async def hello(ctx):
-    await ctx.send(f'Merhaba! Ben {bot.user}, bir Discord sohbet botuyum!')
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('help'):
+        await message.channel.send('çevre kirliliğini nasıl önleriz?,çevre kirliliği nedir?,çevre kirliliği en çok hangi ülkelerde var?,çevre kirliliği devam ederse dünyada neler olur?,gibi şeyler sorabilirsin')
 
-@bot.command()
-async def heh(ctx, count_heh = 5):
-    await ctx.send("he" * count_heh)
+    if message.content.startswith('çevre kirliliğini nasıl önleriz?'):
+        await message.channel.send('yerlere,sulara çöp atmassak önleriz ')
+ 
+    if message.content.startswith('çevre kirliliği nedir?'):
+        await message.channel.send('Çevrenin canlı ve cansız öğelerini olumsuz yönde etkileyen, üzerinde yapısal zararlar meydana getiren ve niteliklerini bozan yabancı maddelerin hava, su ve toprağa yoğun bir şekilde karışması olayına “çevre kirliliği” adı verilmektedir. Çevrenin doğal olmayan bir şekilde insan eliyle bozulmasıdır.')
 
-@bot.command()
-async def naber(ctx):
-    await ctx.send('iyi sen')
+    if message.content.startswith('çevre kirliliği en çok hangi ülkelerde var?'):
+        await message.channel.send('Dünyanın havası en kirli ülkeleri arasında ilk beşi Çad, Irak, Pakistan, Bahreyn ve Bangladeş oluşturdu.')
 
-@bot.command()
-async def kolaysin(ctx):
-    await ctx.send('sen öyle san!')
+    if message.content.startswith('çevre kirliliği devam ederse dünyada neler olur?'):
+        await message.channel.send('Bunlara bağlı olarak enerji kıtlığı,beslenme sorunları, canlı çeşitliliğinin azalması, toprakların kaybı, sağlık problemleri de gelişerek canlı yaşamının devamlılğını tehdit eder. Sadece insanlar için değil birçok kara ve deniz canlısı için de yaşam alanlarının daralmasına sebep olabilmekte.!')
+
